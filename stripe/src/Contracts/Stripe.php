5932be90aa4274f3c1f2c9994cef9b8e624a8562
Christophe Limpalair <christf24@gmail.com>
2015-10-06 18:03:38 -0400

    added functions

--- stripe/src/Contracts/Stripe.php
+++ stripe/src/Contracts/Stripe.php
@@ -4,5 +4,11 @@ namespace Limpalair\Stripe\Contracts;
 
 interface Stripe
 {
-	//
+	public function getStripeKey();
+
+	public function setStripeKey();
+
+	public function getVersion();
+
+	public function setVersion();
 }
