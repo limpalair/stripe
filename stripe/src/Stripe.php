ace7e6f26092c3f03fbbc55113892704af506041
Christophe Limpalair <christf24@gmailcom>
2015-10-06 19:10:39 -0400

    added version number

--- stripe/src/Stripe.php
+++ stripe/src/Stripe.php
@@ -7,6 +7,11 @@ use Limpalair\Stripe\Contracts\Stripe as StripeContract;
 class Stripe
 {
 	/**
+	 * Package version number
+	 */
+	const VERSION = "1.0.0";
+	
+	/**
 	 * The stripe instance
 	 * 
 	 * @var \Limpalair\Stripe\Contracts\StripeContract
