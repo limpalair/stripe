4d4bdc6d1c85706726b38be4a4012cd401744e88
Christophe Limpalair <christf24@gmailcom>
2015-10-06 22:11:10 -0400

    add comment

--- stripe/src/StripeFacade.php
+++ stripe/src/StripeFacade.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Facade;
 
 class StripeFacade extends Facade
 {
+	/**
+	 * Get the registered name of the component
+	 * 
+	 * @return string
+	 */
     protected static function getFacadeAccessor()
     {
         return 'stripeportal';
